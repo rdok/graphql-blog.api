050c62e83963071251149cc5f2b1b1830c462689
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-05 06:58:29 +0000

    Complete math challenge.

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,8 @@
-console.log('alive=================')
+import customlLocation, { message, name, greetings} from './utilities'
+import customlLocation2 from './utilities'
+import sum, { substract } from './math'
+
+console.log(message, name, customlLocation, customlLocation2, greetings('Asterdam'))
+
+console.log(sum(5, 6))
+console.log(substract(5, 6))
---
+++ graphql-basics/src/utilities.js
@@ -0,0 +1,9 @@
+const message = '==========alive================='
+const name = 'Cyberpunk 2077'
+const location = 'Moon'
+
+const greetings = (name) => {
+    return `First man on the moon, ${name}`
+}
+
+export { greetings, message, name, location as default }
