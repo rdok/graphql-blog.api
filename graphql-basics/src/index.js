bcb2231c97273a96bdb762957a2f423d790928ee
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-05 07:33:20 +0000

    Complete challenge on setting up a basic GraphQL query.

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,15 +3,27 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
     type Query {
         hello( name: String): String!
+        name: String!
+        location: String!
+        bio: String!
     }
 `
 
 const resolvers = {
     Query: {
-        hello: (_, { name }) => `Hello ${name || 'World'}`
+        hello: (_, { name }) => `Hello ${name || 'World'}`,
+        name() {
+            return 'name'
+        },
+        location() {
+            return 'loocatoin'
+        },
+        bio() {
+            return 'bio'
+        }
     }
 }
 
-const server =  new GraphQLServer({ typeDefs, resolvers})
+const server = new GraphQLServer({ typeDefs, resolvers })
 
-server.start( () => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log('Server is running on http://localhost:4000'))
