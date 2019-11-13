da95e58e4f3acd05df16bdecdb97d44164ec6362
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-13 20:42:18 +0000

    Create prisma callbacks for creating & update a post.

--- src/prisma.js
+++ src/prisma.js
@@ -1,16 +1,38 @@
-import { Prisma } from 'prisma-binding'
+import {Prisma} from 'prisma-binding'
 
-const prisma =  new Prisma({
+const prisma = new Prisma({
     typeDefs: "src/generated/prisma.graphql",
     endpoint: "http://localhost:4466"
 })
 
-prisma.query.users(null, '{ id name posts { id title  } }')
-    .then((data) => {
-       console.log(JSON.stringify(data, undefined, 2))
-    })
+// prisma.query.users(null, '{ id name posts { id title  } }')
+//     .then((data) => {
+//        console.log(JSON.stringify(data, undefined, 2))
+//     })
+//
+// prisma.query.comments(null, '{ id text author { id email  } post { id title } }')
+//     .then((data) => {
+//         console.log(JSON.stringify(data, undefined, 2))
+//     })
 
-prisma.query.comments(null, '{ id text author { id email  } post { id title } }')
-    .then((data) => {
-        console.log(JSON.stringify(data, undefined, 2))
-    })
+const updatePost = async (id, data) => {
+    return await prisma.mutation.updatePost(
+        {
+            where: { id: id},
+            data: {...data},
+        },
+        ' { id published } '
+    )
+}
+
+updatePost('ck2x1ivkd00ia0719vrizznms', { published: false}).then((data) => {
+    console.log(data)
+})
+
+
+const createPostForUser = async (authorId, data) => {
+    return await prisma.mutation.createPost(
+        {data: {...data, author: {connect: {id: authorId}}}},
+        ' { id title } '
+    )
+}
