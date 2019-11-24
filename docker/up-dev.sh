4b6cee504e18d2dbd4553485d6efa3708cae83d4
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-24 07:27:36 +0000

    Instruct docker to create another database for testing.

--- docker/up-dev.sh
+++ docker/up-dev.sh
@@ -20,9 +20,7 @@ docker-compose-app up --detach
 
 containerName='graphql-blog-api_api_1'
 stateStatus=$(docker inspect -f '{{.State.Status}}' $containerName)
-
 until [ "$stateStatus" == "running" ]; do sleep 1; done
-
 
 docker exec -it $containerName npm install -g prisma
 docker exec -it $containerName npm install
