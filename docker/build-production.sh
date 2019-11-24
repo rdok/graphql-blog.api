d6e1ad3dfa2147cb69ba509225b3405cb294828a
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-24 15:45:42 +0000

    Improve dev docker build speed by moving the prisma to https://hub.docker.com/repository/docker/rdok/prisma-cli/general.

--- docker/build-production.sh
+++ docker/build-production.sh
@@ -8,8 +8,7 @@ PROJECT_DIR="$(
 source "${PROJECT_DIR}/docker/lib.sh"
 
 # Infrastructure docker api image
-docker build --tag rdok/graphql-blog-api:infrastructure . -f "${PROJECT_DIR}/docker/api/Dockerfile.infrastructure"
-# Prisma Image to deploy new schema
-docker build --tag rdok/graphql-blog-api:prisma . -f "${PROJECT_DIR}/docker/api/Dockerfile.prisma"
+docker build --tag rdok/graphql-blog-api:infrastructure . -f "${PROJECT_DIR}/docker/api/Dockerfile-infrastructure"
+docker build --tag rdok/graphql-blog-api:prisma . -f "${PROJECT_DIR}/docker/api/Dockerfile-prisma"
 
 docker-compose-production build
