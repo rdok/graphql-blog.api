4811cb585b0f7f37213e63c848a114d4806561cb
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-30 15:23:51 +0000

    Acceptance test: a guest may be register an account.

--- tests/services/user.test.js
+++ tests/services/user.test.js
@@ -1,18 +1,42 @@
-import {getFirstName} from '../../src/services/user'
+import '@babel/polyfill'
+import 'cross-fetch/polyfill'
+import ApolloBoost, {gql} from 'apollo-boost'
+import prisma from '../../src/prisma'
+
+const client = new ApolloBoost({
+    uri: 'http://api:' + (process.env.TEST_PORT || 4001)
+})
+
+beforeEach(async () => {
+    await prisma.mutation.deleteManyUsers()
+})
 
 describe('User', () => {
-    describe('getFirstName', () => {
-        test('given first and last name', () => {
-            const firstName = getFirstName('Neil Armstrong')
+    test('create new', async () => {
+        const createUser = gql`
+            mutation CreateUser {
+                createUser(data: {
+                    name:"expectedName"
+                    email:"expected@email.test"
+                    password:"cyberpunk2077"
+                })
+                {  user { id name email }  token }
+            }
+        `
 
-            expect(firstName).toBe('Neil')
-        })
+        let user = await prisma.query.user({where: {email: 'expected@email.test'}})
+        expect(user).toBeNull()
 
-        test('given just the first name', () => {
-            const firstName = getFirstName('Bob')
+        const authPayload = await client.mutate({mutation: createUser})
 
-            expect(firstName).toBe('Bob')
+        user = await prisma.query.user(
+            {where: {email: 'expected@email.test'}},
+            '{ email name }'
+        )
 
+        expect(user).toEqual({
+            name: "expectedName",
+            email: "expected@email.test",
         })
     })
 })
